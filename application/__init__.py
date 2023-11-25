efe70d0be8d0aaeea0d5b8cad2c0c8fdc7340270
yazz-1 <alex8salguero@gmail.com>
2023-11-25 19:07:39 +0100

    gitignore

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5569f02c22d1939f85a1530a1c9d330a80f73b7f"
-app.config["MONGO_URI"] = "mongodb+srv://yazz1:sO3Avzbakv80aBAy@cluster0.051rnmh.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://yazz1:<password>@cluster0.051rnmh.mongodb.net/?retryWrites=true&w=majority"
 
 
 #setup database
